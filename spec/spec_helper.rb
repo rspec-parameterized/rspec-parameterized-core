eaff326a062eca9c1c9120ba49b252f5336d8e69
tomykaira <tomykaira@gmail.com>
2012-05-20 10:33:37 +0900

    Modify directory hierarchy with reference to rspec-expectations
    (cherry picked from commit 9625aaae0703de1e425522cfe2484353cddd3087)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,7 @@
-# frozen_string_literal: true
-
-require "rspec/parameterized/core"
+require 'rspec-parameterized'
 
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
-
-  # Disable RSpec exposing methods globally on `Module` and `main`
-  config.disable_monkey_patching!
-
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
-  end
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
+  config.filter_run :current
 end
