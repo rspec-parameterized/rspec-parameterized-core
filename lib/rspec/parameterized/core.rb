54093e57d3726df8c64dc4f59ab980d6c7919eb0
sue445 <sue445@sue445.net>
2022-12-31 12:26:33 +0900

    bundle gem --mit rspec-parameterized-core

---
+++ lib/rspec/parameterized/core.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "core/version"
+
+module Rspec
+  module Parameterized
+    module Core
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
